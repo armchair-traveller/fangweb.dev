d3ebc7210a7fb43918b47e46a0db8d791f9d4038
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-19 05:51:00 -0700

    add a title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,7 @@
 </style>
 
 <svelte:head>
-  <title>TODO: Title</title>
+  <title>Fang's Website · Now With 110% More JAM</title>
   <link
     rel="icon"
     href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='12 0 100 100'><text y='.9em' font-size='90'>🦁</text></svg>" />
