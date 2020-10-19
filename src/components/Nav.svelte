c339c6891b64188a26547a5916ed276910ba120c
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-19 05:40:20 -0700

    add Nav

---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<style>
+  nav {
+    margin-top: 2rem;
+    padding: 0 1.5rem;
+  }
+  h1 {
+    font-weight: 600;
+    font-size: 1.25rem;
+  }
+</style>
+
+<nav>
+  <h1>Fang🦁</h1>
+</nav>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,8 @@
+<!-- Layout is for any layout components like Nav. 
+  Similar to layout in Gatsby, present on all pages -->
 <script>
-  // Reserved for any layout components like Nav. Similar to layout in Gatsby
+  import Nav from "../components/Nav.svelte";
 </script>
 
-<style>
-  /* main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
-  } */
-</style>
-
-<main>
-  <slot />
-</main>
+<Nav />
+<slot />
